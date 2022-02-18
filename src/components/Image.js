2d093d97b9ebe977b885f53f47f51d11eed6638a
Kaike Yudi <kaikeyudi@outlook.com>
2022-02-18 15:41:23 -0300

    requisito 5: cria componente Image

---
+++ src/components/Image.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+  render() {
+    const { src, alt } = this.props;
+    return (
+      <img src={ src } alt={ alt } />
+    );
+  }
+}
+
+export default Image;
